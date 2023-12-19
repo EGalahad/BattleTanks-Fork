3217365c02c79c327ec3abd96fb8d07aa9bf5e7b
elijah <2818195805@qq.com>
2023-12-19 11:40:59 +0800

    Update deploy workflow and base URL configuration

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { World } from "./World/World";
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/BattleTanks-Fork/'
+})
 
 interface CustomWindow extends Window {
   world: any;
