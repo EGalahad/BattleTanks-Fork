6caa2a726a16a09e3cf36c1e81036bb3350a748f
elijah <2818195805@qq.com>
2023-12-19 15:04:47 +0800

    add vite.config.js file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,4 @@
 import { World } from "./World/World";
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/BattleTanks-Fork/'
-})
 
 interface CustomWindow extends Window {
   world: any;
